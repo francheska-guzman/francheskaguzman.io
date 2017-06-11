3dcc4d8b58d8370aa1c38e7739c5553eefb5e28e
gffrancheska <gffrancheska@gmail.com>
2017-06-10 22:15:35 -0400

    Adding footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="wrapper">
-		<h2>Footer</h2>
+      <div id="footer" className="wrapper">
+		<a href="https://github.com/francheska-guzman" target="_blank" >
+		  <img className="social" src="images/github.png" /></a>
+		<a href="https://linkedin.com/in/francheska-guzman/" target="_blank" >
+		  <img className="social" src="images/linkedin.png" /></a>
 	  </div>
     );
   }
