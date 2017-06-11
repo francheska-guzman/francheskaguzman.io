0cbc85ec564d22efe2e478cec4f8a4ec152aad25
gffrancheska <gffrancheska@gmail.com>
2017-06-10 22:18:35 -0400

    Updating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div id="footer" className="wrapper">
+      <div className="wrapper">
+      <footer>
 		<a href="https://github.com/francheska-guzman" target="_blank" >
 		  <img className="social" src="images/github.png" /></a>
 		<a href="https://linkedin.com/in/francheska-guzman/" target="_blank" >
 		  <img className="social" src="images/linkedin.png" /></a>
+	  </footer>
 	  </div>
     );
   }
