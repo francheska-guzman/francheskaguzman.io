69ec06b00a6e8cdd9598a886dec34c545549f76b
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-15 12:30:34 -0400

    Updating

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import About from './components/About';
-import Contact from './components/Contact';
 import FourOFour from './components/Four_O_Four';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Projects from './components/Projects';
 import Navigation from './components/Navigation';
 import {
         BrowserRouter as Router,
@@ -18,7 +15,16 @@ console.log("App.js is working.");
 class App extends Component {
   render() {
     return (
-      <div></div>
+        <Router>
+	      <div>
+	        <Route startsWith path="/portfolio/" component={() => (<Navigation />) }/>
+	        <Switch>
+	          <Route path="/portfolio/" exact component={() => (<Home />) }/>
+	          <Route path="/portfolio/*" component={() => (<FourOFour />) }/>
+	        </Switch>
+	        <Route startsWith path="/portfolio/" component={() => (<Footer />) }/>
+	      </div>
+        </Router>
     );
   }
 }
--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 class Navigation extends Component {
   render() {
     return (
-      <div id="navigation">This is Navigation component.</div>
+      <div id="navigation">
+
+        <a href='#projects'>Projects</a>
+        <a href='#about'>About</a>
+        <a href='#contact'>Contact</a>
+
+        <ScrollableAnchor id={'projects'}><Projects /></ScrollableAnchor>
+        <ScrollableAnchor id={'about'}><About /></ScrollableAnchor>
+        <ScrollableAnchor id={'contact'}><Contact /></ScrollableAnchor>
+        
+      </div>
     );
   }
 }
