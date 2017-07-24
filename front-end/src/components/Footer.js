05e33be6ed7ac3aae53747934af6803818bed1df
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-23 22:22:33 -0400

    Open in new tab.

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -4,9 +4,9 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-      	<a href="https://github.com/francheska-guzman"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
-      	<a href="https://www.linkedin.com/in/francheska-guzman"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
-      	<a href="https://twitter.com/gffrancheska"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
+      	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
+      	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
+      	<a href="https://twitter.com/gffrancheska" target="_black"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
       </footer>
     );
   }
