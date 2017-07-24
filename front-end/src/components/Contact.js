de4bba51326005beb18f39914f5247d3ab1a4194
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-24 08:48:46 -0400

    Updating

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
       <div id='contact' className='flex-col'>
         <h3>Contact</h3>
         <h6>E-mail: gffrancheska@gmail.com</h6>
-        <h6>Phone Number: 787-410-9414</h6>
+        <h6>Telephone Number: 787-410-9414</h6>
         <h6>Resume: <a href="https://francheska-guzman.github.io/" target="_blank">Click Here</a></h6>
       </div>
     );
