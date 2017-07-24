9221ac82c1830c79481fc85530116dcb34f8860c
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-23 22:53:38 -0400

    Adding logo.

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -6,6 +6,7 @@ class Footer extends Component {
       <footer>
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
+      	<a href="https://profiles.generalassemb.ly/profiles/francheska-guzman" target="_black"><img className="logo" src="./images/logo/ga.png" alt="General Assembly" /></a>
       	<a href="https://www.utest.com/profile/francheska-guzman/about" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
       </footer>
     );
