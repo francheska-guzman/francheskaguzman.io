931b060ec9407e8442af72159a4b6ecf1a61076e
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-23 21:51:13 -0400

    Adding logos to footer.

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <footer></footer>
+      <footer>
+      	<a href="https://github.com/francheska-guzman"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
+      	<a href="https://www.linkedin.com/in/francheska-guzman/"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
+      	<a href="https://twitter.com/gffrancheska"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
+      </footer>
     );
   }
 }
