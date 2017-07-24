8eabc0a959527d0f05c9609b88fbfae589d2b927
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-23 23:03:05 -0400

    Updating

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -4,10 +4,10 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-      	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
-      	<a href="https://linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
-      	<a href="https://profiles.generalassemb.ly/profiles/francheska-guzman" target="_black"><img className="logo" src="./images/logo/ga.png" alt="General Assembly" /></a>
-      	<a href="https://utest.com/profile/francheska-guzman/about" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
+      	<a href="https://www.github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
+      	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
+      	<a href="https://www.profiles.generalassemb.ly/profiles/francheska-guzman" target="_black"><img className="logo" src="./images/logo/ga.png" alt="General Assembly" /></a>
+      	<a href="https://www.utest.com/profile/francheska-guzman/about" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
       </footer>
     );
   }
