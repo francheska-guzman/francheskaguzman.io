810c8bccabfa0729718b3ea46426d2c11de67604
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-25 09:34:34 -0400

    Updating

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div id='home' className="vertical-center">
-      	<h1>Francheska Guzman</h1>
-      	<h2>A web developer based in New York.</h2>
+      <div id='home'>
+        <div className="vertical-center">
+      	  <h1>Francheska Guzman</h1>
+      	  <h2>A web developer based in New York.</h2>
+      	  <br/>
+      	  <a href="#about"><img alt="Check out my portfolio!" id="anchor" src="./images/logo/anchor.png" /></a>
+      	</div>
       </div>
     );
   }
