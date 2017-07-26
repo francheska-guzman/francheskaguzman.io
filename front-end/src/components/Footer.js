eb73253443a769f50e2e03498a2f7e39cfeb0249
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-25 21:09:55 -0400

    Updating

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <h6 className="bone-white">Handmade by <a className="bone-white" href="http://francheskaguzman.io">Francheska Guzman</a> © 2017 All Rights Reserved.</h6>
+        <h6 className="bone-white">Handmade by <a className="bone-white" href="http://francheskaguzman.io">Francheska Guzman</a>.</h6>
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
         {/*<a href="https://profiles.generalassemb.ly/profiles/francheska-guzman" target="_black"><img className="logo" src="./images/logo/ga.png" alt="General Assembly" /></a>*/}
