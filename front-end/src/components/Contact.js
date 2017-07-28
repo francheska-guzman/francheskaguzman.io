78278fb45b4cee7cec2952badc4438cb55ef5253
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-28 18:17:16 -0400

    Updating

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -5,6 +5,8 @@ class Contact extends Component {
     return (
       <div id='contact'>
         <h3>Contact</h3>
+        <h6>Let's do people loves to navigate the web applications even more!</h6>
+        <br/>
         <h6>Francheska Guzman</h6>
         <h6>E-mail: gffrancheska@gmail.com</h6>
         <h6>Telephone Number: 787-410-9414</h6>
