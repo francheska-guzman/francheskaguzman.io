1ac5bc695298908c04fa2e3e6e5b8664eb8a755a
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-28 12:15:39 -0400

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -5,6 +5,9 @@ class About extends Component {
     return (
       <div id='about'>
         <h3>About</h3>
+        <p className="about-me">I am a passionate web developer who grow up in Puerto Rico, and currently lives in New York. I worked over seven years providing accounting services within the higher education sector, and collaborating as a software tester.</p>
+        <br/> 
+		<p className="about-me">Most recently, in June 2017, I completed an intensive Web Development training program from Per Scholas and General Assembly, where I gained knowledge on how to develop real world web applications from design to coding.</p>
       </div>
     );
   }
