8f8a8213d6fdca12a150fa363f3eacc11f28f786
Francheska <francheska-guzman@users.noreply.github.com>
2017-08-02 12:50:35 -0400

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -10,7 +10,7 @@ class About extends Component {
         </p>
         <br />
         <p className="paragraph">
-        As a recently graduated student from the Web Development Immersive Program at General Assembly, I am very excited to put my knowledge and skills into practice to develop tools for our society.
+        As a recently graduated student from the Web Development Immersive program at General Assembly, I am very excited to put my knowledge and skills into practice to develop tools for our society.
         I am currently looking for opportunities as a web developer entry-level position in New York or remotely. Check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>!
         </p>
       </div>
