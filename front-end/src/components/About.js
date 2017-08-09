51a439159769cb2fb8c59e0817118a89ef844d44
Francheska <francheska-guzman@users.noreply.github.com>
2017-08-09 08:52:27 -0400

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -5,13 +5,9 @@ class About extends Component {
     return (
       <div id='about'>
         <h3>About</h3>
-        <p className="paragraph">
-        I am a passionate web developer based in New York, with over seven years of experience providing accounting services within the higher education sector. I am a member of a crowdsourced software testing community called uTest. 
-        </p>
+        <p className="paragraph">I am a passionate web developer based in New York, who have a background in accounting. I am a member of a crowdsourced software testing community. I am actively learning new technologies with a can-do attitude.</p>
         <br />
-        <p className="paragraph">
-        As a recently graduated student from the Web Development Immersive program at General Assembly, I am very excited to put my knowledge and skills into practice to develop tools for our society.
-        I am currently looking for opportunities as a web developer entry-level position in New York or remotely. Check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>!
+        <p className="paragraph">As a recently graduated student from the Web Development Immersive program at General Assembly, I am very excited to put my knowledge and skills into practice to develop tools for our society. I am currently looking for opportunities as a web developer entry-level position in New York or remotely. Check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>!
         </p>
       </div>
     );
