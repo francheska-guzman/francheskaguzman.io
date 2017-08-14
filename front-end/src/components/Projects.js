f692c8996b52729819c5155798751d63528ee4b4
Francheska <francheska-guzman@users.noreply.github.com>
2017-08-13 20:00:59 -0400

    Updating

--- front-end/src/components/Projects.js
+++ front-end/src/components/Projects.js
@@ -10,16 +10,16 @@ class Projects extends Component {
       description: "A financial technology application that calculates the six credit factors based on user’s financial information contained in the Credit Report API.",
       instructions: "Type '1' or '2' to get user information in the provided input field, which is located in the navigation bar. The application calculate the results in base of the user information. Try using both numbers so you can see that both users have different results.",
       technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "PostgreSQL"],
-      url_live: "https://francheska-guzman.github.io/credit-report",
-      url_code: "https://github.com/francheska-guzman/credit-report",
-      url_api: "https://creditreportapi.herokuapp.com",
+      live: "https://francheska-guzman.github.io/credit-report",
+      code: "https://github.com/francheska-guzman/credit-report",
+      api: "https://creditreportapi.herokuapp.com",
       slideshow: ["./images/credit-report/1.png", "./images/credit-report/2.png", "./images/credit-report/3.png"]
     }
     this.project = this.project.bind(this);
-    this.titleDescription = this.titleDescription.bind(this);
-    this.titleInstructions = this.titleInstructions.bind(this);
-    this.titleTechnologiesUsed = this.titleTechnologiesUsed.bind(this);
+    this.description = this.description.bind(this);
+    this.instructions = this.instructions.bind(this);
     this.technologies = this.technologies.bind(this);
+    this.list = this.list.bind(this);
     this.live = this.live.bind(this);
     this.code = this.code.bind(this);
     this.api = this.api.bind(this);
@@ -27,58 +27,66 @@ class Projects extends Component {
     this.slideshow = this.slideshow.bind(this);
   }
 
-  project(title, description, instructions, technologies, url_live, url_code, url_api, slideshow) {
+  project(title, description, instructions, technologies, live, code, api, slideshow) {
     this.setState({
       title: title,
       description: description,
       instructions: instructions,
       technologies: technologies,
-      url_live: url_live,
-      url_code: url_code,
-      url_api: url_api,
+      live: live,
+      code: code,
+      api: api,
       slideshow: slideshow
     });
   }
 
-  titleDescription() {
-    if(this.state.description !== "") { return( <h5>Description</h5> ) }
+  /* Render title if no empty. */
+
+  description() {
+    if(this.state.description !== "") { return( <h5>Description</h5> ) };
   }
 
-  titleInstructions() {
-    if(this.state.instructions !== "") { return( <h5>Instructions</h5> ) }
-  }
-
-  titleTechnologiesUsed() {
-    if(this.state.technologies[0] !== null) { return( <h5>Technologies Used</h5> ) }
+  instructions() {
+    if(this.state.instructions !== "") { return( <h5>Instructions</h5> ) };
   }
 
   technologies() {
+    if(this.state.technologies[0] !== null) { return( <h5>Technologies Used</h5> ) };
+  }
+
+  /* Creating a dynamic list of technologies used. */
+
+  list() {
     return (this.state.technologies.map(function(technology, t) { 
       return ( <li className="project" key={t + 1}>{technology}</li> ) })
     )
   }
+ 
+  /* Render links. */
 
   live() {
-    if(this.state.url_live !== "") {
-      return ( <a href={this.state.url_live} target="_blank"><img alt="Live" className="img-link" src="./images/logo/live.png" /></a> );
+    if(this.state.live !== "") {
+      return ( <a href={this.state.live} target="_blank"><img alt="Live" className="img-link" src="./images/logo/live.png" /></a> );
     }
   }
 
   code() {
-    if(this.state.url_code !== "") {
-      return ( <a href={this.state.url_code} target="_blank"><img alt="Code" className="img-link" src="./images/logo/code.png" /></a> );
+    if(this.state.code !== "") {
+      return ( <a href={this.state.code} target="_blank"><img alt="Code" className="img-link" src="./images/logo/code.png" /></a> );
     }
   }
 
   api() {
-    if(this.state.url_api !== "") {
-      return ( <a href={this.state.url_api} target="_blank"><img alt="API"  className="img-link"  src="./images/logo/api.png" /></a> );
+    if(this.state.api !== "") {
+      return ( <a href={this.state.api} target="_blank"><img alt="API"  className="img-link"  src="./images/logo/api.png" /></a> );
     }
   }
 
   buttons() {
     return ( <div className="right-and-center"> {this.live()} {this.code()} {this.api()} </div> );
   }
+
+  /* Pictures for each project. */
 
   slideshow() {
     if(this.state.slideshow[0] === null) {
@@ -109,16 +117,16 @@ class Projects extends Component {
         <div className="flex-row">
           <div className='flex-1'> {this.slideshow()} </div>
           <div className="flex-1 project-information">
-            {this.titleDescription()}
+            {this.description()}
             <p>{this.state.description}</p>
             <div className='flex-row'>
               <div className='flex-1'> 
-                {this.titleInstructions()}
+                {this.instructions()}
                 <p>{this.state.instructions}</p> 
               </div>
               <div className='flex-1 project-information'> 
-                {this.titleTechnologiesUsed()} 
                 {this.technologies()} 
+                {this.list()} 
               </div>
             </div>
           </div>
