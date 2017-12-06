a0a160cc6786b162f21f941cfb4b44a50e865fe9
Francheska <Francheska@Franciscos-iMac.home>
2017-12-06 15:10:54 -0500

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         <h3>About</h3>
         <p className="paragraph">I am a passionate and detail-oriented web developer based in New York, who have a background in accounting. I am a member of a crowdsourced software testing community called uTest. I am actively learning new technologies with a can-do attitude.</p>
         <br />
-        <p className="paragraph">I am currently looking for opportunities as a web developer entry-level position in New York or remotely. Check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>!
+        <p className="paragraph">I am currently looking for opportunities as a web developer entry-level position in New York, Puerto Rico, or remotely. Check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>!
         </p>
       </div>
     );
