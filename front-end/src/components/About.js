85e337aa15b0da5e49441c4d4e5b67c09d5d402f
Francheska <francheska-guzman@users.noreply.github.com>
2017-12-13 16:43:59 -0500

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         <h3>About</h3>
         <p className="paragraph">Hello everyone!</p><br/>
         <p className="paragraph">
-        My name is Francheska Guzman, and I am a full-stack web developer who loves learning new technologies. Occasionally, I contribute to the <a href="https://www.utest.com/profile/francheska-guzman/about" target="_blank" rel="noopener noreferrer">uTest</a> community through executing test cases, as well performing exploratory and functional testing in web and native applications. I am currently looking for opportunities as a web developer entry level/intern position in New York or remote. Feel free to check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>, and more projects on <a href="https://github.com/francheska-guzman" target="_blank" rel="noopener noreferrer">GitHub</a>!
+        My name is Francheska Guzman, and I am a web developer who loves learn new technologies. Occasionally, I contribute to the <a href="https://www.utest.com/profile/francheska-guzman/about" target="_blank" rel="noopener noreferrer">uTest</a> community through executing test cases, as well performing exploratory and functional testing in web and native applications. I am currently looking for opportunities as a web developer entry level/intern position in New York or remote. Feel free to check out my <a href="https://francheska-guzman.github.io/" target="_blank" rel="noopener noreferrer">resume</a>, and more projects on <a href="https://github.com/francheska-guzman" target="_blank" rel="noopener noreferrer">GitHub</a>!
         </p>
       </div>
     );
