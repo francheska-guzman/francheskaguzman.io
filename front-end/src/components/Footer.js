53fc698c3f74841cacc773b6d8e7b54fa2b5c7e3
Francheska <francheska-guzman@users.noreply.github.com>
2017-12-14 07:43:34 -0500

    Updating

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
         <a href="http://francheskaguzman.io"><img className="logo" src="./images/logo/portfolio.png" alt="Portfolio" /></a>
-        <a href="https://www.utest.com/profile/francheska-guzman/about" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
+        <a href="https://www.utest.com/profile/francheska-guzman/stats" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
         <a href="https://twitter.com/gffrancheska" target="_black"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
       </footer>
     );
