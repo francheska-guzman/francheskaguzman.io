dd4d2b10cbdca4b331f75960f27e29f12baa125a
Francheska <francheska-guzman@users.noreply.github.com>
2017-12-14 08:05:03 -0500

    Updating

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
       <div id='about'>
         <h3>About</h3>
         <p className="paragraph">Hello everyone!</p><br/>
-        <p className="paragraph">My name is Francheska and I was born and raised in San Juan, Puerto Rico. In 2015, after graduating from college, I moved to New York. I am a passionate web developer who loves learn new technologies. Eager to contribute to the society, and grow in the tech field. Feel free to checkout my <a href="https://francheska-guzman.github.io" target="_blank">resume</a> for more details regarding my experience.</p>
+        <p className="paragraph">My name is Francheska and I was born and raised in San Juan, Puerto Rico. In 2015, after graduating from college, I moved to New York. I am a passionate web developer, and a member of a crowdsourced software testing community called <a href="https://www.utest.com/profile/francheska-guzman/stats" target="_blank">uTest</a>. Eager to contribute to the society, and grow in the tech field. I am currently open for an entry-level or intern position, so feel free to checkout my <a href="https://francheska-guzman.github.io" target="_blank">resume</a>! </p>
       </div>
     );
   }
