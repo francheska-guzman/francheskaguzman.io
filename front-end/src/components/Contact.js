390dadf3c31c7a95872c207c8391b1f599bc7938
Francheska <francheska-guzman@users.noreply.github.com>
2018-01-20 08:47:06 -0500

    Updating

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -4,13 +4,19 @@ class Contact extends Component {
   render() {
     return (
       <div id='contact'>
-        <h3>Contact</h3>
-        <h6>Let’s make people love using web applications even more!</h6>
-        <br/>
-        <h6>Francheska Guzman</h6>
-        <h6>E-mail: gffrancheska@gmail.com</h6>
-        <h6>Telephone: 787-410-9414</h6>
-        <h6><a href="https://francheska-guzman.github.io" rel="noopener noreferrer" target="_blank">Resume</a></h6>
+        <div className="flex-row">
+          <div className="flex-2">
+            <h3>Contact</h3>
+            <h5>Let’s make people love using web applications even more!</h5>
+            <br/>
+            <h6>Francheska Guzman</h6>
+            <h6>E-mail: gffrancheska@gmail.com</h6>
+            <h6>Telephone: 787-410-9414</h6>
+          </div>
+          <div className="flex-1 resume">
+            <a className="resume-link" href="https://francheska-guzman.github.io" rel="noopener noreferrer" target="_blank">Resume</a>
+          </div>
+        </div>
       </div>
     );
   }
