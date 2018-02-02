271ad8537596a53d90aba06e150eb85553bc4db3
Francheska <francheska-guzman@users.noreply.github.com>
2018-02-02 16:07:34 -0500

    Adding eighth note color.

--- src/components/MiniProjects.js
+++ src/components/MiniProjects.js
@@ -9,24 +9,24 @@ class MiniProjects extends Component {
 
         <p>Yey! You have found more treasures!</p>
         <br/>
-        <p>&#x266A; Means that the project contains audio effect. Not supported in Safari browser.</p>
+        <p>The eighth note <span className="eighth-note">&#x266A;</span> means that the project contains audio effect. Not supported in Safari browser.</p>
 
         <div className="mini-projects-center">
           <div className="flexrow space-20">
             <div className="space-1 flex-1">
-              <span className="p-title flex-1">Velvet &#x266A;</span>
+              <span className="p-title flex-1">Velvet <span className="eighth-note">&#x266A;</span></span>
               <a className="p1 flex-1" href="https://francheska-guzman.github.io/velvet" target="_black">
                   <img className="slide-size" alt="Velvet" src="./images/velvet/1.png" />
               </a>
             </div>
             <div className="space-2 flex-1">
-              <span className="p-title flex-1">Haunted House &#x266A;</span>
+              <span className="p-title flex-1">Haunted House <span className="eighth-note">&#x266A;</span></span>
               <a className="p2 flex-1" href="https://francheska-guzman.github.io/haunted-house/" target="_black">
                   <img className="slide-size" alt="Francheska's Haunted House" src="./images/haunted-house/1.png" />
               </a>
             </div>
             <div className="space-3 flex-1">
-              <span className="p-title flex-1">Rio &#x266A;</span>
+              <span className="p-title flex-1">Rio <span className="eighth-note">&#x266A;</span></span>
               <a className="p3 flex-1" href="https://francheska-guzman.github.io/rio/" target="_black">
                   <img className="slide-size" alt="Rio" src="./images/rio/1.png" />
               </a>
@@ -35,7 +35,7 @@ class MiniProjects extends Component {
 
           <div className="flexrow space-20">
             <div className="space-1 flex-1">
-              <span className="p-title flex-1">The Secret Life of Math &#x266A;</span>
+              <span className="p-title flex-1">The Secret Life of Math <span className="eighth-note">&#x266A;</span></span>
               <a className="p1 flex-1" href="https://francheska-guzman.github.io/the-secret-life-of-math/" target="_black">
                   <img className="slide-size" alt="The Secret Life of Math" src="./images/the-secret-life-of-math/1.png" />
               </a>
