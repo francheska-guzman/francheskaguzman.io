f2f9fb9431828160b6b7fcf48c126db43aac7cd5
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-06 08:00:38 -0400

    Updating

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,8 @@ class About extends Component {
         <p className="paragraph">
         Hi everyone!
         <br/><br/>
-        My name is Francheska, I am from Puerto Rico and living in New York since 2015. 
-        I am a passionate web developer who loves to learn new technologies and devise solutions to problems. 
-        Continuously looking to improve the quality of my work, and pushing myself to the next level. 
-        My previous experience as an accountant allows me to quickly understand how financial-related applications needs to work,
-        and their impact in the real world, as well meet deadlines. I am currently a member of a software testing community called uTest.
+        My name is Francheska, and I am a passionate web developer who loves to learn new technologies and devise solutions to problems. 
+        Continuously looking to improve the quality of my work, and pushing myself to the next level. I am currently looking for a full time position as a Front End Developer or Full Stack Web Developer.
         </p>
       </div>
     );
