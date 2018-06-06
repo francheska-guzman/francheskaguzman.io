9f775e3c3f5c4fb51d88b0bce6fc917967e97443
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-06 17:43:59 -0400

    Updating url.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         <h6 className="grey">Handmade by <a href="https://github.com/francheska-guzman">Francheska Guzman</a>.</h6>
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github-white.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
-        <a href="http://francheska.io"><img className="logo" src="./images/logo/portfolio.png" alt="Portfolio" /></a>
+        <a href="https://francheska.io"><img className="logo" src="./images/logo/portfolio.png" alt="Portfolio" /></a>
         <a href="https://www.utest.com/profile/francheska-guzman/stats" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
         <a href="https://twitter.com/gffrancheska" target="_black"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
       </footer>
