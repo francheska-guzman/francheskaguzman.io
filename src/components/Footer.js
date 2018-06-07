dc1201e4ebca3c2d6f64f93080c47944cde799bb
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-07 11:20:33 -0400

    Updating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github-white.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
         <a href="https://francheska.io"><img className="logo" src="./images/logo/portfolio.png" alt="Portfolio" /></a>
-        <a href="utest.com/profile/francheska-guzman" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
+        <a href="http://utest.com/profile/francheska-guzman" target="_black"><img className="logo" src="./images/logo/utest.png" alt="uTest" /></a>
         <a href="https://twitter.com/gffrancheska" target="_black"><img className="logo" src="./images/logo/twitter.png" alt="Twitter" /></a>
       </footer>
     );
