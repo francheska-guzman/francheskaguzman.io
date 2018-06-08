501b25081e90d67b8fa54cc772b508f0b5d409c7
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-08 06:14:35 -0400

    Updating

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,9 +9,9 @@ class Contact extends Component {
         <h5>Let’s make people love using web applications even more!</h5>
         <br/>
             <h6>Francheska Guzman</h6>
+            <h6>Phone Number: 787-410-9414</h6>
             <h6>E-mail: gffrancheska@gmail.com</h6>
             <h6>Skype: francheska-guzman@outlook.com</h6>
-            <h6>Telephone: 787-410-9414</h6>
             <h6><a href = {PDF} target = "_blank">Resume</a></h6>
       </div>
     );
