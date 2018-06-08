5fd3a2c72508a359761d8792410da914da5dc31c
Francheska <francheska-guzman@users.noreply.github.com>
2018-06-08 04:10:53 -0400

    Updating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <h6 className="grey">Handmade by <a href="https://github.com/francheska-guzman">Francheska Guzman</a>.</h6>
+        <h6 className="grey">Handmade by <a href="https://github.com/francheska-guzman" target="_black">Francheska Guzman</a>.</h6>
       	<a href="https://github.com/francheska-guzman" target="_black"><img className="logo" src="./images/logo/github-white.png" alt="GitHub" /></a>
       	<a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="logo" src="./images/logo/linkedin.png" alt="LinkedIn" /></a>
         <a href="https://francheska.io"><img className="logo" src="./images/logo/portfolio.png" alt="Portfolio" /></a>
